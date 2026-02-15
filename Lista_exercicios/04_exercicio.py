5b5bd06571533231ed6d5598cc04cf95854633c8
Pedro Marques <pedro.marques@ics.ufal.br>
2026-02-15 20:17:46 -0300

    atuaizado

--- Lista_exercicios/04_exercicio.py
+++ Lista_exercicios/04_exercicio.py
@@ -57,9 +57,6 @@ f = ((9 * celsius) / 5) + 32
 print(f"A temperatura {celsius} em Fahrenheit é: {f}")
 
 # %% Exercicio 3.14
-# 60 reais por dia
-# 0.15 reais por km
-
 kmrodado = float(input("Entre a quantidade de quilometro rodado: "))
 dia = int(input("Entre com a quantidade de dias: "))
 
--- Lista_exercicios/05_exercicio.py
+++ Lista_exercicios/05_exercicio.py
@@ -19,9 +19,6 @@ else:
     print(f"Maior número: {c}")
 
 # %% Excercicio 4.4
-# salárioacima de 1250 aumento de 10%
-# inferiores ou iguais, acumento de 15%
-
 salario = float(input("Entre com seu salário: "))
 
 if salario > 1250:
@@ -32,3 +29,47 @@ elif salario <= 1250:
     aumento = salario * 0.15
     salario2 = aumento + salario
     print(f"Com aumento de 15%, seu salário de {salario}, foi para {salario2}")
+
+# %% Exercicio 4.5
+idade = int(input("Digite a idade do seu carro: "))
+
+if idade <= 3:
+    print("Seu carro é novo")
+else:
+    print("Seu carro é velho")
+
+# %% Exercicio 4.6
+
+distancia = int(input("Digite a distância que você quer percorrer(em km): "))
+
+if distancia <= 200:
+    preco = distancia * 0.50
+    print(f"O preço a ser pago: {preco}")
+else:
+    preco = distancia * 0.45
+    print(f"O preço a ser pago: {preco}")
+
+# %% Excercicio 4.7
+categoria = int(input("Digite a categória do produto: "))
+
+if categoria == 1:
+    proco = 10
+else:
+    if categoria == 2:
+        preco = 18
+    else:
+        if categoria == 3:
+            preco = 23
+        else:
+            if categoria == 4:
+                preco = 26
+            else:
+                if categoria == 5:
+                    preco = 31
+                else:
+                    print("Categoria inválida, digite um valor entre 1 e 5!")
+                    preco = 0
+                
+print(f"O preço do produto é: R${preco}")
+
+# %% 
