2f37b744120a91b3ee80d9b06aa27a1c8c83c67b
Pedro Marques <pedro.marques@ics.ufal.br>
2026-02-15 13:59:40 -0300

    atualizado

--- Lista_exercicios/05_exercicio.py
+++ Lista_exercicios/05_exercicio.py
@@ -5,3 +5,15 @@ velocidade = int(input("Entre com a velocidade do carro: "))
 if velocidade > 80:
     multa = (velocidade - 80) * 5
     print(f"Você recemeu uma multa de: {multa}")
+
+# %% Exercicio 4.3
+a = int(input("Entre com o primeiro número: "))
+b = int(input("Entre com o segundo número: "))
+c = int(input("Entre com o terceiro número: "))
+
+if a > b and a > c:
+    print(f"Maior número: {a}")
+elif b > a and b > c:
+    print(f"Maior número: {b}")
+else:
+    print(f"Maior número: {c}")
