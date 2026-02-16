91bff6511e076f08545a871b36ee4496c6f7d11c
Pedro Marques <pedro.marques@ics.ufal.br>
2026-02-16 10:24:50 -0300

    atualizado

---
+++ Lista_exercicios/06_exercicio.py
@@ -0,0 +1,21 @@
+# %% Exercicio 5.1
+x = 1
+
+while x <= 100:
+    print(x)
+    x += 1
+
+# %% Exercicio 5.2
+x = 50
+
+while x <= 100:
+    print(x)
+    x += 1
+
+# %% Exercicio 5.3
+x = 10
+
+while x > 0:
+    print(x)
+    x -= 1
+print("fogo")
