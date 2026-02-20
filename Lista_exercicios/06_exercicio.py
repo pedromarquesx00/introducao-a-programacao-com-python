c38220cc5265ff0595cba884132e7276c70c1a2f
Pedro Marques <pedro.marques@ics.ufal.br>
2026-02-20 15:39:57 -0300

    atualizado

--- Lista_exercicios/06_exercicio.py
+++ Lista_exercicios/06_exercicio.py
@@ -63,6 +63,51 @@ while x <= fim:
     x = x + 1
 
 # %% Exercicio 5.8
+n = int(input("Tabuada de: "))
 a = int(input("Entre com o primeiro número: "))
 b = int(input("Entre com o segundo número: "))
 
+if a <= b:
+    x = a
+    while x <= b:
+        soma1 = (f"{n} + " * (x - 1) + f"{n}").strip()
+        soma2 = (f"{x} + " * (n - 1) + f"{x}").strip()
+        print(f"{n} x {x} = {soma1} = {soma2} = {n * x}")
+        x = x + 1
+    
+else:
+    print("Erro")
+
+# %% Exercicio 5.9
+dividendo = int(input("Entre com o dividendo: "))
+divisor = int(input("Entre com o divisor: "))
+
+if divisor == 0:
+    print("Não se faz divisão por zero!")
+else:
+    quociente = 0
+    resto = dividendo
+
+while resto >= divisor:
+    resto = resto - divisor
+    quociente = quociente + 1
+
+print(f"Divisão inteira: {dividendo} ÷ {divisor} = {quociente}")
+print(f"Resto: {resto}")
+
+# %% Exercicio 5.10
+pontos = 0
+questao = 1
+
+while questao <= 3:
+    resposta = input(f"Resposta da questão: {questao}")
+    if questao == 1 and (resposta == "B" or resposta == "b"):
+        pontos += 1
+    if questao == 2 and (resposta == "A" or resposta == "a"):
+        pontos += 1
+    if questao == 3 and (resposta == "D" or resposta == "d"):
+        pontos += 1
+    questao += 1
+print(f"O aluno fez {pontos} ponto(s)")
+
+# %%
