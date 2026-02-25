9937773c63aa698a5d23c509653840806b7bd1f5
Pedro Marques <pedro.marques@ics.ufal.br>
2026-02-25 17:01:20 -0300

    atualizado

--- Lista_exercicios/07_exercicio.py
+++ Lista_exercicios/07_exercicio.py
@@ -12,3 +12,18 @@ while x < 7:
     print("Nota %d: %6.2f" % (x, notas[x]))
     x += 1
 print("Média: %6.5f" % (soma/x))
+
+# %% Listagem 6.7
+numero = [0,0,0,0,0]
+x = 0
+
+while x < 5:
+    numero[x] = int(input(f"Número: {x + 1}"))
+    x += 1
+while True:
+    escolhido = int(input("Que posição você quer imprimir (o para sair): "))
+    if escolhido == 0:
+        break
+    print("Você escolheu o número: %d" % (numero[escolhido-1]))
+
+# %%
