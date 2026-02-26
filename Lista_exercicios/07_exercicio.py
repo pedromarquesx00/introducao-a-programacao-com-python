91a1bac5172d27c0655b9afbc4f8c4eaf25ddb9b
Pedro Marques <pedro.marques@ics.ufal.br>
2026-02-26 16:34:28 -0300

    atualizado

--- Lista_exercicios/07_exercicio.py
+++ Lista_exercicios/07_exercicio.py
@@ -42,4 +42,31 @@ k = [1, 5, 6, 7, 8, 8, 9]
 j = list(set(k + l))
 j
 
-# %%
+# %% Exercicio 6.5
+ultimo = 10
+fila = list(range(1, ultimo + 1))
+
+while True:
+    print("\nExistem %d clientes na fila" % len(fila))
+    print("Fila atual:", fila)
+    print("Digite F para adicionar um cliente ao fim da fila,")
+    print("ou A para realizar o atendimento. S para sair")
+    operacao = input("Operação (F, A ou S):").upper()
+
+    if operacao == "A":
+        if len(fila) > 0:
+            atendido = fila.pop(0)
+            print("Cliente %d atendido" % atendido)
+    
+        else:
+            print("Fila vazia! Ninguém para atender.")
+    
+    elif operacao == "F":
+        ultimo += 1 
+        fila.append(ultimo)
+    
+    elif operacao == "S":
+        break
+    
+    else:
+        print("Operação inválida. Digite apenas F, A ou S!")
